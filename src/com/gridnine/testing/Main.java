375c62cebe768e0c89758b68300b1dbf2f059c8b
Eugene <sirius-tyrin@yandex.ru>
2021-03-19 19:27:19 +0300

    ver 0.2

--- src/com/gridnine/testing/Main.java
+++ src/com/gridnine/testing/Main.java
@@ -7,16 +7,21 @@
 package com.gridnine.testing;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 public class Main {
     static List<Flight> approvedFlights;
-    static List<Flight> excludedFlights;
+    static List<Flight> flightsDepartingInThePast;
+    static List<Flight> flightsDepartsBeforeArrives;
+    static List<Flight> flightsTwoHoursGroundTime;
 
     public static void main(String[] args) {
-        approvedFlights = null;
-        excludedFlights = null;
+        approvedFlights = new ArrayList<>();
+        flightsDepartingInThePast = new ArrayList<>();
+        flightsDepartsBeforeArrives = new ArrayList<>();
+        flightsTwoHoursGroundTime = new ArrayList<>();
 
         List<Flight> testflights = FlightBuilder.createFlights();   // создание тестового набора перелётов
         checkFlight(testflights);                                   // выдает результат обработки набора
@@ -27,17 +32,40 @@ public class Main {
         while (iterator.hasNext()) {
             Flight flight = (Flight) iterator.next();
             for (Segment segment : flight.getSegments()) {
-                if (segment.getArrivalDate().isBefore(LocalDateTime.now()) || segment.getDepartureDate().isBefore(LocalDateTime.now())) {
-                    excludedFlights.add(flight);
-                } else {
-                    approvedFlights.add(flight);
+                if (checkFlightDepartingInThePast(segment)){
+                    flightsDepartingInThePast.add(flight);
+                    break;
                 }
+                if (checkFlightDepartsBeforeArrives(segment)){
+                    flightsDepartsBeforeArrives.add(flight);
+                    break;
+                }
+                if (checkFlightTwoHoursGroundTime(segment)){
+                    flightsTwoHoursGroundTime.add(flight);
+                    break;
+                }
+                approvedFlights.add(flight);
             }
         }
         showAllFlights();
     }
 
-    static void checkNextSegment() {
+    static boolean checkFlightDepartingInThePast(Segment segment) {
+        if (segment.getDepartureDate().isBefore(LocalDateTime.now())) {
+            return true;
+        }
+        return false;
+    }
+
+    static boolean checkFlightDepartsBeforeArrives(Segment segment) {
+        if (segment.getArrivalDate().isBefore(segment.getDepartureDate())) {
+            return true;
+        }
+        return false;
+    }
+
+    static boolean checkFlightTwoHoursGroundTime(Segment segment) {
+        return false;
     }
 
     static void showAllFlights() {
@@ -46,10 +74,25 @@ public class Main {
         while (flightIterator.hasNext()) {
             System.out.println(flightIterator.next());
         }
-        System.out.println("------ Отменённые рейсы ------");
-        Iterator<Flight> flight = excludedFlights.iterator();
-        while (flight.hasNext()) {
-            System.out.println(flight.next());
+//        System.out.println("------ Отменённые рейсы ------");
+//        Iterator<Flight> flight = excludedFlights.iterator();
+//        while (flight.hasNext()) {
+//            System.out.println(flight.next());
+//        }
+        System.out.println("------ Вылет до текущего момента ------");
+        Iterator<Flight> flight1 = flightsDepartingInThePast.iterator();
+        while (flight1.hasNext()) {
+            System.out.println(flight1.next());
+        }
+        System.out.println("------ Дата прилёта раньше даты вылета ------");
+        Iterator<Flight> flight2 = flightsDepartsBeforeArrives.iterator();
+        while (flight2.hasNext()) {
+            System.out.println(flight2.next());
+        }
+        System.out.println("------ Время между сегментами более двух часов ------");
+        Iterator<Flight> flight3 = flightsTwoHoursGroundTime.iterator();
+        while (flight3.hasNext()) {
+            System.out.println(flight3.next());
         }
     }
 }
